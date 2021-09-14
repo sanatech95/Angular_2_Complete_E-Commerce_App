45fb98f50e5c15ab0ca22c2d209134bb4bc6dba8
sanatech95 <tasneem786786.khan@gmail.com>
2021-09-14 12:00:51 +0530

    Angular_2_E-Commerce

---
+++ bookstore-angular/src/main/java/com/bookstore/service/impl/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.bookstore.service.impl;
+
+import java.util.Set;
+
+import javax.transaction.Transactional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.domain.User;
+import com.bookstore.domain.security.UserRole;
+import com.bookstore.repository.RoleRepository;
+import com.bookstore.repository.UserRepository;
+import com.bookstore.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+	
+	private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Transactional
+	public User createUser(User user, Set<UserRole> userRoles) {
+		User localUser = userRepository.findByUsername(user.getUsername());
+		
+		if(localUser != null) {
+			LOG.info("User with username {} already exit. Nothing will be done. ", user.getUsername());
+		} else {
+			for (UserRole ur : userRoles) {
+				roleRepository.save(ur.getRole());
+			}
+			user.getUserRoles().addAll(userRoles);
+			
+			localUser = userRepository.save(user);
+		}
+		
+		return localUser;
+		
+	}
+
+}
